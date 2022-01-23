4978342f0141994c6a1420efda260e3989ecb717
Hassan Mohamed <HassanMohamed_HM@hotmail.com>
2022-01-23 16:14:25 +0200

    Create Solution

---
+++ TrickingLibirary.Api/Extensions/ServicesExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace TrickingLibirary.Api.Extensions;
+
+public static class ServicesExtensions
+{
+    public static IServiceCollection AddWebServices(this IServiceCollection services)
+    {
+        services.AddRazorPages();
+        services.AddControllers();
+        services.AddCors(options =>
+        options.AddPolicy("All", build =>
+           build.AllowAnyHeader()
+           .AllowAnyOrigin()
+           .AllowAnyMethod()
+          ));
+        return services;
+    }
+}
